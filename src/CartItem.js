bbc76609a0996b124bede93a21a3c068645e955b
Nikhil <ns345360@gmail.com>
2022-11-05 14:11:57 +0530

    added state to CartItem

--- src/CartItem.js
+++ src/CartItem.js
@@ -2,19 +2,35 @@ import React from "react";
 
 //class 'CartItem' inherits features from class 'Component' inside the 'React' package 
 class CartItem extends React.Component {
+    constructor() {
+        super(); //calling the constructor of parent class is mandatory in the inheriting class
+        //defining state for CartItem component 
+        this.state = {
+            price: 999,
+            title: 'Mobile Phone',
+            qty: 1,
+            img: ''
+        }
+    }
+
     //for returning jsx from a class, we have to use this render function
     render() {
+        const {price, title, qty} = this.state; //object destructuring, this.state is defined above in the constructor
+
         return (
             <div className="cart-item">
                 <div className="left-block">
                     <img style={styles.image} />
                 </div>
                 <div className="right-block">
-                    <div style={ {fontSize: 25} }>Phone</div>
-                    <div style={ {color: '#777'} }>Rs 999</div>
-                    <div style={ {color: '#777'} }>Qty: 1</div>
+                    <div style={ { fontSize: 25 } }>{title}</div>
+                    <div style={ { color: '#777' } }>Rs {price}</div>
+                    <div style={ { color: '#777' } }>Qty: {qty}</div>
                     <div className="cart-item-actions">
                         {/* Buttons */}
+                        <img alt="increase" className="action-icons" src="https://cdn-icons-png.flaticon.com/128/992/992651.png" />
+                        <img alt="decrease" className="action-icons" src="https://cdn-icons-png.flaticon.com/128/992/992683.png" />
+                        <img alt="delete" className="action-icons" src="https://cdn-icons-png.flaticon.com/128/1214/1214428.png" />
                     </div>
                 </div>
             </div>
