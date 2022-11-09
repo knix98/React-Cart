32ca591d1ec307c395f1d1deeaee16dfeb1c3f12
Nikhil <ns345360@gmail.com>
2022-11-09 13:14:28 +0530

    added images of products

--- src/App.js
+++ src/App.js
@@ -14,21 +14,21 @@ class App extends React.Component {
                 price: 999,
                 title: 'Mobile Phone',
                 qty: 1,
-                img: '',
+                img: 'https://images.unsplash.com/photo-1601784551446-20c9e07cdbdb?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mnx8bW9iaWxlJTIwcGhvbmV8ZW58MHx8MHx8&auto=format&fit=crop&w=500&q=60',
                 id: 1
             },
             {
                 price: 99,
                 title: 'Camera',
                 qty: 10,
-                img: '',
+                img: 'https://images.unsplash.com/photo-1516035069371-29a1b244cc32?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mnx8Y2FtZXJhfGVufDB8fDB8fA%3D%3D&auto=format&fit=crop&w=500&q=60',
                 id: 2
             },
             {
                 price: 999,
                 title: 'Laptop',
                 qty: 4,
-                img: '',
+                img: 'https://images.unsplash.com/photo-1496181133206-80ce9b88a853?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mnx8bGFwdG9wfGVufDB8fDB8fA%3D%3D&auto=format&fit=crop&w=500&q=60',
                 id: 3
             }
         ]
